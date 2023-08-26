aea74e7996c537458a4347c5e5964afc8ec74f33
qiu_lijie <qiu_lijie@ymail.com>
2023-08-26 07:34:03 -0700

    contract stub

---
+++ eve_monitor/contract_sniper.py
@@ -0,0 +1,7 @@
+import logging
+import requests
+
+
+def watch_contract(s: requests.Session):
+    """watch for low priced low volume contract"""
+    return
--- tasks.py
+++ tasks.py
@@ -4,18 +4,27 @@ import time
 import traceback
 
 from eve_monitor.constants import SETTINGS
+from eve_monitor.contract_sniper import watch_contract
 from eve_monitor.market_monitor import watch_market
 
 
 logging.basicConfig(format='%(asctime)s %(levelname)s\t%(message)s', level=logging.INFO)
 
 s = requests.Session()
+FEATURES = SETTINGS['features_enabled']
 while True:
     try:
-        if SETTINGS['features_enabled']['market_monitor']:
+        if FEATURES['market_monitor']:
             watch_market(s)
-    except Exception:
+    except:
         logging.error('Unexpected error occurred during market watch:')
+        traceback.print_exc()
+
+    try:
+        if FEATURES['contract_sniper']:
+            watch_contract(s)
+    except:
+        logging.error('Unexpected error occurred during contract watch:')
         traceback.print_exc()
 
     logging.info('----sleep----')
